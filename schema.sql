e63d5491c0f749187d1176b07f31125b00c94c43
Juuso Kosonen <kosonenjusu@gmail.com>
2026-03-26 11:49:19 +0200

    added seed data for boardgame categories

--- schema.sql
+++ schema.sql
@@ -1,22 +1,22 @@
 CREATE TABLE IF NOT EXISTS Users (
-	id	INTEGER,
-	username VARCHAR(100) NOT NULL UNIQUE,
-	password VARCHAR(255) NOT NULL,
+    id INTEGER,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
     avatar BLOB,
-	PRIMARY KEY(id AUTOINCREMENT)
+    PRIMARY KEY(id AUTOINCREMENT),
 );
 
 CREATE TABLE IF NOT EXISTS Boardgames (
-	id	INTEGER,
-	name VARCHAR(100) NOT NULL UNIQUE,
+    id INTEGER,
+    name VARCHAR(100) NOT NULL UNIQUE,
     description TEXT,
     number_of_players INTEGER,
-    type_id INTEGER,
+    category_id INTEGER,
     duration INTEGER,
-    free_games INTEGER DEFAULT 1
-    reserved_games INTEGER DEFAULT 0
-	PRIMARY KEY(id AUTOINCREMENT),
-    FOREIGN KEY (type_id) REFERENCES Categories(id),
+    free_games INTEGER DEFAULT 1,
+    reserved_games INTEGER DEFAULT 0,
+    PRIMARY KEY(id AUTOINCREMENT),
+    FOREIGN KEY (category_id) REFERENCES Categories(id),
 );
 
 CREATE TABLE IF NOT EXISTS Categories (
@@ -25,8 +25,8 @@ CREATE TABLE IF NOT EXISTS Categories (
 )
 
 CREATE TABLE IF NOT EXISTS Photos (
-    boardgame_id INTEGER NOT NULL
-    photo BLOB NOT NULL
+    boardgame_id INTEGER NOT NULL,
+    photo BLOB NOT NULL,
 )
 
 
@@ -37,15 +37,15 @@ CREATE TABLE IF NOT EXISTS Ratings (
     review TEXT,
     PRIMARY KEY(boardgame_id),
     FOREIGN KEY (boardgame_id) REFERENCES Boardgames(id),
-    FOREIGN KEY (user_id) REFERENCES Users(id)
+    FOREIGN KEY (user_id) REFERENCES Users(id),
 )
 
 CREATE TABLE IF NOT EXISTS Reservation (
-    start_time VARCHAR(19) 
-    end_time VARCHAR(19)
-    user_id INTEGER REFERENCES
-    boardgame_id REFERENCES
-    PRIMARY KEY(user_id)
+    start_time VARCHAR(19),
+    end_time VARCHAR(19),
+    user_id INTEGER REFERENCES,
+    boardgame_id REFERENCES,
+    PRIMARY KEY(user_id),
     FOREIGN KEY (boardgame_id) REFERENCES Boardgames(id),
-    FOREIGN KEY (user_id) REFERENCES Users(id)
+    FOREIGN KEY (user_id) REFERENCES Users(id),
 )
