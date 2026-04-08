5c28e3204a7df6c27b94320be8e540da2f5ccaed
Juuso Kosonen <kosonenjusu@gmail.com>
2026-04-08 16:39:16 +0300

    review datatype

--- datatypes.py
+++ datatypes.py
@@ -7,6 +7,7 @@ class User(UserMixin):
     username: str
     password: str
 
+
 class Boardgame:
     def __init__(self, name: str, number_of_players: int , duration: int, id: int | None = None, description: int | None = None, category_id: int | None = None, free_games: int | None = None, reserved_games: int | None = None):
         self.name = name
@@ -18,50 +19,6 @@ class Boardgame:
         self.free_games = free_games
         self.reserved_games = reserved_games
 
-    @property
-    def category(self, category: str):
-        match category:
-            case "Lasten peli":
-                self.category_id = 1
-            case "Korttipeli":
-                self.category_id = 2
-            case "Pakohuonepeli":
-                self.category_id = 3
-            case "Yhteistyöpeli":
-                self.category_id = 4
-            case "Sotapeli":
-                self.category_id = 5
-            case "Partypeli":
-                self.category_id = 6
-            case "Tietopeli":
-                self.category_id = 7
-            case "Perhepeli":
-                self.category_id = 8
-            case _:
-                self.category_id = 0
-    
-    @category.getter
-    def category(self) -> str:
-        match self.category_id:
-            case 1:
-                return "Lasten peli"
-            case 2:
-                return "Korttipeli"
-            case 3:
-                return "Pakohuonepeli"
-            case 4:
-                return "Yhteistyöpeli"
-            case 5:
-                return "Sotapeli"
-            case 6:
-                return "Partypeli"
-            case 7:
-                return "Tietopeli"
-            case 8:
-                return "Perhepeli"
-            case _:
-                return "Muu"
-
     def from_form(form) -> 'Boardgame':
         return Boardgame(
             form["name"],
@@ -71,3 +28,77 @@ class Boardgame:
             category_id=form["category_id"],
             free_games=form["free_games"],
         )
+
+class Review:
+    def __init__(self, user: str, review: str | None, stars: float):
+        self.user = user
+        self.review = review
+        self.rating = stars
+        self.stars = int(stars)
+        self.half = False
+
+        frac = stars - self.stars
+        if 0.25 <= frac <= 0.75:
+            self.half = True
+        elif frac > 0.75 and self.stars <= 5:
+            self.stars += 1
+
+    def __add__(self, other) -> float:
+        if isinstance(other, Review):
+            return self.rating + other.rating
+        elif isinstance(other, int):
+            return self.rating + other
+        elif isinstance(other, float):
+            return self.rating + other
+        else:
+            return ValueError(f"Can't add Review and {type(other)}")
+
+    def __sub__(self, other) -> float:
+        if isinstance(other, Review):
+            return self.rating - other.rating
+        elif isinstance(other, int):
+            return self.rating - other
+        elif isinstance(other, float):
+            return self.rating - other
+        else:
+            return ValueError(f"Can't subtract Review and {type(other)}")
+
+    def __mul__(self, other) -> float:
+        if isinstance(other, Review):
+            return self.rating * other.rating
+        elif isinstance(other, int):
+            return self.rating * other
+        elif isinstance(other, float):
+            return self.rating * other
+        else:
+            return ValueError(f"Can't multiple Review and {type(other)}")
+
+    def __truediv__(self, other) -> float:
+        if isinstance(other, Review):
+            return self.rating / other.rating
+        elif isinstance(other, int):
+            return self.rating / other
+        elif isinstance(other, float):
+            return self.rating / other
+        else:
+            return ValueError(f"Can't divide Review and {type(other)}")
+
+    def __floordiv__(self, other) -> float:
+        if isinstance(other, Review):
+            return self.rating // other.rating
+        elif isinstance(other, int):
+            return self.rating // other
+        elif isinstance(other, float):
+            return self.rating // other
+        else:
+            return ValueError(f"Can't floor divide Review and {type(other)}")
+
+    def __pow__(self, other) -> float:
+        if isinstance(other, Review):
+            return self.rating ** other.rating
+        elif isinstance(other, int):
+            return self.rating ** other
+        elif isinstance(other, float):
+            return self.rating ** other
+        else:
+            return ValueError(f"Can't get power Review and {type(other)}")
