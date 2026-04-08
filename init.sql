99c4ea58edb18c6ac5a490e5ad9938139cd1d011
Juuso Kosonen <kosonenjusu@gmail.com>
2026-04-08 16:35:34 +0300

    fixed init.sql

--- init.sql
+++ init.sql
@@ -1,9 +1,9 @@
-INSERT OR IGNORE Category VALUES (0, "Muu");
-INSERT OR IGNORE Category VALUES (1, "Lasten peli");
-INSERT OR IGNORE Category VALUES (2, "Korttipeli");
-INSERT OR IGNORE Category VALUES (3, "Pakohuonepeli");
-INSERT OR IGNORE Category VALUES (4, "Yhteistyöpeli");
-INSERT OR IGNORE Category VALUES (5, "Sotapeli");
-INSERT OR IGNORE Category VALUES (6, "Partypeli");
-INSERT OR IGNORE Category VALUES (7, "Tietopeli");
-INSERT OR IGNORE Category VALUES (8, "Perhepeli");
+INSERT OR IGNORE INTO Categories VALUES (0, "Muu");
+INSERT OR IGNORE INTO Categories VALUES (1, "Lasten peli");
+INSERT OR IGNORE INTO Categories VALUES (2, "Korttipeli");
+INSERT OR IGNORE INTO Categories VALUES (3, "Pakohuonepeli");
+INSERT OR IGNORE INTO Categories VALUES (4, "Yhteistyöpeli");
+INSERT OR IGNORE INTO Categories VALUES (5, "Sotapeli");
+INSERT OR IGNORE INTO Categories VALUES (6, "Partypeli");
+INSERT OR IGNORE INTO Categories VALUES (7, "Tietopeli");
+INSERT OR IGNORE INTO Categories VALUES (8, "Perhepeli");
