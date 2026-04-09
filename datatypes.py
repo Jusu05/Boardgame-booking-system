46a0d1f1f0a2b32354950c945c76e296fffc95aa
Juuso Kosonen <kosonenjusu@gmail.com>
2026-04-09 04:05:36 +0300

    added review showing

--- datatypes.py
+++ datatypes.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from security import UserMixin
 
+
 @dataclass
 class User(UserMixin):
     id: int
@@ -9,15 +10,18 @@ class User(UserMixin):
 
 
 class Boardgame:
-    def __init__(self, name: str, number_of_players: int , duration: int, id: int | None = None, description: int | None = None, category_id: int | None = None, free_games: int | None = None, reserved_games: int | None = None):
+    def __init__(self, name: str, number_of_players: int, duration: int, id: int | None = None, description: int | None = None, free_games: int | None = None, reserved_games: int | None = None, category: str | None = None, category_id: int | None = None, stars: int | None = None, half_star: bool | None = None):
         self.name = name
         self.description = description
         self.number_of_players = number_of_players
         self.duration = duration
         self.id = id
-        self.category_id = category_id
         self.free_games = free_games
         self.reserved_games = reserved_games
+        self.category = category
+        self.category_id = category_id
+        self.stars = stars
+        self.half_star = half_star
 
     def from_form(form) -> 'Boardgame':
         return Boardgame(
@@ -25,23 +29,19 @@ class Boardgame:
             form["number_of_players"],
             form["duration"],
             description=form["description"],
-            category_id=form["category_id"],
             free_games=form["free_games"],
+            category_id=form["category_id"],
         )
 
-class Review:
-    def __init__(self, user: str, review: str | None, stars: float):
-        self.user = user
-        self.review = review
-        self.rating = stars
-        self.stars = int(stars)
-        self.half = False
 
-        frac = stars - self.stars
-        if 0.25 <= frac <= 0.75:
-            self.half = True
-        elif frac > 0.75 and self.stars <= 5:
-            self.stars += 1
+class Review:
+    def __init__(self, user: str, text: str | None, rating: float, stars: int | None, half_star: bool | None, user_avatar):
+        self.user = user
+        self.user_avatar = user_avatar
+        self.text = text
+        self.rating = rating
+        self.stars = stars
+        self.half_star = half_star
 
     def __add__(self, other) -> float:
         if isinstance(other, Review):
@@ -51,7 +51,17 @@ class Review:
         elif isinstance(other, float):
             return self.rating + other
         else:
-            return ValueError(f"Can't add Review and {type(other)}")
+            raise ValueError(f"Can't add Review and {type(other)}")
+
+    def __radd__(self, other) -> float:
+        if isinstance(other, Review):
+            return other.rating + self.rating
+        elif isinstance(other, int):
+            return other + self.rating
+        elif isinstance(other, float):
+            return other + self.rating
+        else:
+            raise ValueError(f"Can't add Review and {type(other)}")
 
     def __sub__(self, other) -> float:
         if isinstance(other, Review):
@@ -61,7 +71,17 @@ class Review:
         elif isinstance(other, float):
             return self.rating - other
         else:
-            return ValueError(f"Can't subtract Review and {type(other)}")
+            raise ValueError(f"Can't subtract Review and {type(other)}")
+
+    def __rsub__(self, other) -> float:
+        if isinstance(other, Review):
+            return other.rating - self.rating
+        elif isinstance(other, int):
+            return other - self.rating
+        elif isinstance(other, float):
+            return other - self.rating
+        else:
+            raise ValueError(f"Can't subtract Review and {type(other)}")
 
     def __mul__(self, other) -> float:
         if isinstance(other, Review):
@@ -71,7 +91,10 @@ class Review:
         elif isinstance(other, float):
             return self.rating * other
         else:
-            return ValueError(f"Can't multiple Review and {type(other)}")
+            raise ValueError(f"Can't multiple Review and {type(other)}")
+
+    def __rmul__(self, other) -> float:
+        return self.__mul__(other)
 
     def __truediv__(self, other) -> float:
         if isinstance(other, Review):
@@ -81,7 +104,17 @@ class Review:
         elif isinstance(other, float):
             return self.rating / other
         else:
-            return ValueError(f"Can't divide Review and {type(other)}")
+            raise ValueError(f"Can't divide Review and {type(other)}")
+
+    def __rtruediv__(self, other) -> float:
+        if isinstance(other, Review):
+            return other.rating / self.rating
+        elif isinstance(other, int):
+            return other / self.rating
+        elif isinstance(other, float):
+            return other / self.rating
+        else:
+            raise ValueError(f"Can't divide Review and {type(other)}")
 
     def __floordiv__(self, other) -> float:
         if isinstance(other, Review):
@@ -91,7 +124,17 @@ class Review:
         elif isinstance(other, float):
             return self.rating // other
         else:
-            return ValueError(f"Can't floor divide Review and {type(other)}")
+            raise ValueError(f"Can't floor divide Review and {type(other)}")
+
+    def __rfloordiv__(self, other) -> float:
+        if isinstance(other, Review):
+            return other.rating // self.rating
+        elif isinstance(other, int):
+            return other // self.rating
+        elif isinstance(other, float):
+            return other // self.rating
+        else:
+            raise ValueError(f"Can't floor divide Review and {type(other)}")
 
     def __pow__(self, other) -> float:
         if isinstance(other, Review):
@@ -101,4 +144,14 @@ class Review:
         elif isinstance(other, float):
             return self.rating ** other
         else:
-            return ValueError(f"Can't get power Review and {type(other)}")
+            raise ValueError(f"Can't get power Review and {type(other)}")
+
+    def __rpow__(self, other) -> float:
+        if isinstance(other, Review):
+            return other.rating ** self.rating
+        elif isinstance(other, int):
+            return other ** self.rating
+        elif isinstance(other, float):
+            return other ** self.rating
+        else:
+            raise ValueError(f"Can't get power Review and {type(other)}")
