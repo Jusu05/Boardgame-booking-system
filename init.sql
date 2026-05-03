99532ba375c89e4419c9f5d2e20cceb4f8ac985c
Juuso Kosonen <kosonenjusu@gmail.com>
2026-05-03 21:08:40 +0300

    give up course for this period

--- init.sql
+++
@@ -1,9 +0,0 @@
-INSERT OR IGNORE INTO categories VALUES (0, "Muu");
-INSERT OR IGNORE INTO categories VALUES (1, "Lasten peli");
-INSERT OR IGNORE INTO categories VALUES (2, "Korttipeli");
-INSERT OR IGNORE INTO categories VALUES (3, "Pakohuonepeli");
-INSERT OR IGNORE INTO categories VALUES (4, "Yhteistyöpeli");
-INSERT OR IGNORE INTO categories VALUES (5, "Sotapeli");
-INSERT OR IGNORE INTO categories VALUES (6, "Partypeli");
-INSERT OR IGNORE INTO categories VALUES (7, "Tietopeli");
-INSERT OR IGNORE INTO categories VALUES (8, "Perhepeli");
